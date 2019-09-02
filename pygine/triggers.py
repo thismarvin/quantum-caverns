46c3543abdcf24f825578af18187af1c4b01f710
thismarvin <bend_@outlook.com>
2019-09-02 16:49:20 -0500

    added new OnButtonPress trigger

--- pygine/triggers.py
+++ pygine/triggers.py
@@ -2,7 +2,7 @@ from enum import IntEnum
 from pygine.base import PygineObject
 from pygine.draw import draw_rectangle
 from pygine.entities import Direction, Player
-from pygine.utilities import InputType
+from pygine.input import pressed, InputType
 
 
 class Trigger(PygineObject):
@@ -11,12 +11,15 @@ class Trigger(PygineObject):
         self.next_scene = next_scene
         self.end_location = end_location
 
+    def _queue_next_scene(self, manager):
+        manager.queue_next_scene(self.next_scene)
+
     def _move_entity_to_next_scene(self, entity, manager):
         next_scene = manager.get_scene(self.next_scene)
         current_scene = manager.get_current_scene()
 
         if isinstance(entity, Player):
-            manager.queue_next_scene(self.next_scene)
+            self._queue_next_scene(manager)
             next_scene.relay_actor(entity)
         else:
             next_scene.relay_entity(entity)
@@ -24,9 +27,9 @@ class Trigger(PygineObject):
         current_scene.entities.remove(entity)
         entity.set_location(self.end_location.x, self.end_location.y)
 
-    def update(self, delta_time, entities, entity_quad_tree, manager):
+    def update(self, delta_time, scene_data, manager):
         raise NotImplementedError(
-            "A class that inherits Trigger did not implement the update(delta_time, entities, entity_quad_tree, manager) method")
+            "A class that inherits Trigger did not implement the update(delta_time, scene_data, manager) method")
 
     def draw(self, surface, camera_type):
         raise NotImplementedError(
@@ -40,14 +43,14 @@ class CollisionTrigger(Trigger):
         self.direction = direction
         self.query_result = None
 
-    def __collision(self, entities, entity_quad_tree, manager):
-        self.query_result = entity_quad_tree.query(self.bounds)
+    def __collision(self, scene_data, manager):
+        self.query_result = scene_data.entity_quad_tree.query(self.bounds)
         for e in self.query_result:
             if e.bounds.colliderect(self.bounds):
                 self._move_entity_to_next_scene(e, manager)
 
-    def update(self, delta_time, entities, entity_quad_tree, manager):
-        self.__collision(entities, entity_quad_tree, manager)
+    def update(self, delta_time, scene_data, manager):
+        self.__collision(scene_data, manager)
 
     def draw(self, surface, camera_type):
         draw_rectangle(
@@ -57,25 +60,15 @@ class CollisionTrigger(Trigger):
         )
 
 
-class ButtonTrigger(Trigger):
-    def __init__(self, x, y, width, height, end_location, next_scene, direction=Direction.UP):
-        super(ButtonTrigger, self).__init__(
-            x, y, width, height, end_location, next_scene)
-        self.direction = direction
-        self.query_result = None
+class OnButtonPressTrigger(Trigger):
+    def __init__(self, button, end_location, next_scene):
+        super(OnButtonPressTrigger, self).__init__(
+            0, 0, 1, 1, end_location, next_scene)
+        self.button = button
 
-    def __collision(self, entities, entity_quad_tree, manager):
-        self.query_result = entity_quad_tree.query(self.bounds)
-        for e in self.query_result:
-            if e.bounds.colliderect(self.bounds):
-                if isinstance(e, Player):
-                    if e.input.pressing(InputType.A) and int(e.facing) == int(self.direction):
-                        self._move_entity_to_next_scene(e, manager)
-                else:
-                    self._move_entity_to_next_scene(e, manager)
-
-    def update(self, delta_time, entities, entity_quad_tree, manager):
-        self.__collision(entities, manager, entity_quad_tree)
+    def update(self, delta_time, scene_data, manager):
+        if pressed(self.button):
+            self._queue_next_scene(manager)
 
     def draw(self, surface, camera_type):
         draw_rectangle(
